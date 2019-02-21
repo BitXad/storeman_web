c39554d5408d27a7454a6dba6daf4292b081f2a0
Mario escobar <escobar_m_98@hotmail.com>
2019-02-21 17:12:25 -0400

    Pedido RUD

--- application/views/pedido/index.php
+++ application/views/pedido/index.php
@@ -1,43 +1,73 @@
+<!------------------ ESTILO DE LAS TABLAS ----------------->
+<link href="<?php echo base_url('resources/css/mitabla.css'); ?>" rel="stylesheet">
+<!-------------------------------------------------------->
+<div class="box-header">
+    <h3 class="box-title">Pedido</h3>
+    <div class="box-tools">
+        <a href="<?php echo site_url('pedido/add'); ?>" class="btn btn-success btn-sm">+ Añadir</a> 
+    </div>
+</div>
 <div class="row">
     <div class="col-md-12">
         <div class="box">
-            <div class="box-header">
-                <h3 class="box-title">Pedido Listing</h3>
-            	<div class="box-tools">
-                    <a href="<?php echo site_url('pedido/add'); ?>" class="btn btn-success btn-sm">Add</a> 
-                </div>
-            </div>
+            
             <div class="box-body">
-                <table class="table table-striped">
+                <table class="table table-striped" id="mitabla">
                     <tr>
-						<th>Pedido Id</th>
-						<th>Estado Id</th>
-						<th>Gestion Id</th>
-						<th>Pedido Fecha</th>
-						<th>Pedido Hora</th>
-						<th>Pedido Archivo</th>
-						<th>Pedido Imagen</th>
-						<th>Pedido Numero</th>
-						<th>Pedido Fechapedido</th>
-						<th>Actions</th>
+                        <th>#</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Archivo</th>
+                        <th>Imagen</th>
+                        <th>Número</th>
+                        <th>Fecha Pedido</th>
+                        <th>Gestión</th>
+                        <th>Estado</th>
+                        <th></th>
                     </tr>
-                    <?php foreach($pedido as $p){ ?>
+                    <?php
+                        $i = 0;
+                        foreach($pedido as $p){ ?>
                     <tr>
-						<td><?php echo $p['pedido_id']; ?></td>
-						<td><?php echo $p['estado_id']; ?></td>
-						<td><?php echo $p['gestion_id']; ?></td>
-						<td><?php echo $p['pedido_fecha']; ?></td>
-						<td><?php echo $p['pedido_hora']; ?></td>
-						<td><?php echo $p['pedido_archivo']; ?></td>
-						<td><?php echo $p['pedido_imagen']; ?></td>
-						<td><?php echo $p['pedido_numero']; ?></td>
-						<td><?php echo $p['pedido_fechapedido']; ?></td>
-						<td>
-                            <a href="<?php echo site_url('pedido/edit/'.$p['pedido_id']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Edit</a> 
-                            <a href="<?php echo site_url('pedido/remove/'.$p['pedido_id']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Delete</a>
+                        
+                        <td><?php echo $i+1; ?></td>
+                        <td><?php echo $p['pedido_fecha']; ?></td>
+                        <td><?php echo $p['pedido_hora']; ?></td>
+                        <td><?php echo $p['pedido_archivo']; ?></td>
+                        <td><?php echo $p['pedido_imagen']; ?></td>
+                        <td><?php echo $p['pedido_numero']; ?></td>
+                        <td><?php echo $p['pedido_fechapedido']; ?></td>
+                        <td><?php echo $p['gestion_id']; ?></td>
+                        <td><?php echo $p['estado_id']; ?></td>
+                        <td>
+                            <a href="<?php echo site_url('pedido/edit/'.$p['pedido_id']); ?>" class="btn btn-info btn-xs" title="Editar"><span class="fa fa-pencil"></span></a>
+                            <a data-toggle="modal" data-target="#myModal<?php echo $i; ?>"  title="Eliminar" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span></a>
+                            <!------------------------ INICIO modal para confirmar eliminación ------------------->
+                            <div class="modal fade" id="myModal<?php echo $i; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel<?php echo $i; ?>">
+                              <div class="modal-dialog" role="document">
+                                    <br><br>
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">x</span></button>
+                                  </div>
+                                  <div class="modal-body">
+                                   <!------------------------------------------------------------------->
+                                   <h3><b> <span class="fa fa-trash"></span></b>
+                                       ¿Desea eliminar el Pedido <b> <?php echo $p['pedido_numero']; ?></b>?
+                                   </h3>
+                                   <!------------------------------------------------------------------->
+                                  </div>
+                                  <div class="modal-footer aligncenter">
+                                              <a href="<?php echo site_url('pedido/remove/'.$p['pedido_id']); ?>" class="btn btn-success"><span class="fa fa-check"></span> Si </a>
+                                              <a href="#" class="btn btn-danger" data-dismiss="modal"><span class="fa fa-times"></span> No </a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                        <!------------------------ FIN modal para confirmar eliminación ------------------->
                         </td>
                     </tr>
-                    <?php } ?>
+                    <?php $i++; } ?>
                 </table>
                                 
             </div>
