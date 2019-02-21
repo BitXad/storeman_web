5aae66fd380d1fed8d0cdc78208667d5f04adeb8
Mario escobar <escobar_m_98@hotmail.com>
2019-02-21 12:41:25 -0400

    Institución CRUD

--- application/views/institucion/add.php
+++ application/views/institucion/add.php
@@ -2,55 +2,56 @@
     <div class="col-md-12">
       	<div class="box box-info">
             <div class="box-header with-border">
-              	<h3 class="box-title">Institucion Add</h3>
+              	<h3 class="box-title">Añadir Institución</h3>
             </div>
             <?php echo form_open('institucion/add'); ?>
           	<div class="box-body">
           		<div class="row clearfix">
 					<div class="col-md-6">
-						<label for="institucion_nombre" class="control-label">Institucion Nombre</label>
+						<label for="institucion_nombre" class="control-label"><span class="text-danger">*</span>Nombre</label>
 						<div class="form-group">
-							<input type="text" name="institucion_nombre" value="<?php echo $this->input->post('institucion_nombre'); ?>" class="form-control" id="institucion_nombre" />
+							<input type="text" name="institucion_nombre" value="<?php echo $this->input->post('institucion_nombre'); ?>" class="form-control" id="institucion_nombre" required />
+                                                        <span class="text-danger"><?php echo form_error('institucion_nombre');?></span>
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="institucion_sucursal" class="control-label">Institucion Sucursal</label>
+						<label for="institucion_sucursal" class="control-label">Sucursal</label>
 						<div class="form-group">
 							<input type="text" name="institucion_sucursal" value="<?php echo $this->input->post('institucion_sucursal'); ?>" class="form-control" id="institucion_sucursal" />
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="institucion_direccion" class="control-label">Institucion Direccion</label>
+						<label for="institucion_direccion" class="control-label">Dirección</label>
 						<div class="form-group">
 							<input type="text" name="institucion_direccion" value="<?php echo $this->input->post('institucion_direccion'); ?>" class="form-control" id="institucion_direccion" />
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="institucion_ubicacion" class="control-label">Institucion Ubicacion</label>
+						<label for="institucion_ubicacion" class="control-label">Ubicación</label>
 						<div class="form-group">
 							<input type="text" name="institucion_ubicacion" value="<?php echo $this->input->post('institucion_ubicacion'); ?>" class="form-control" id="institucion_ubicacion" />
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="institucion_telef" class="control-label">Institucion Telef</label>
+						<label for="institucion_telef" class="control-label">Teléfono</label>
 						<div class="form-group">
 							<input type="text" name="institucion_telef" value="<?php echo $this->input->post('institucion_telef'); ?>" class="form-control" id="institucion_telef" />
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="institucion_nit" class="control-label">Institucion Nit</label>
+						<label for="institucion_nit" class="control-label">Nit</label>
 						<div class="form-group">
 							<input type="text" name="institucion_nit" value="<?php echo $this->input->post('institucion_nit'); ?>" class="form-control" id="institucion_nit" />
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="institucion_autorizacion" class="control-label">Institucion Autorizacion</label>
+						<label for="institucion_autorizacion" class="control-label">Autorización</label>
 						<div class="form-group">
 							<input type="text" name="institucion_autorizacion" value="<?php echo $this->input->post('institucion_autorizacion'); ?>" class="form-control" id="institucion_autorizacion" />
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="institucion_eslogan" class="control-label">Institucion Eslogan</label>
+						<label for="institucion_eslogan" class="control-label">Eslogan</label>
 						<div class="form-group">
 							<input type="text" name="institucion_eslogan" value="<?php echo $this->input->post('institucion_eslogan'); ?>" class="form-control" id="institucion_eslogan" />
 						</div>
@@ -59,8 +60,10 @@
 			</div>
           	<div class="box-footer">
             	<button type="submit" class="btn btn-success">
-            		<i class="fa fa-check"></i> Save
+                    <i class="fa fa-check"></i>Guardar
             	</button>
+                <a href="<?php echo site_url('institucion'); ?>" class="btn btn-danger">
+                    <i class="fa fa-times"></i> Cancelar</a>
           	</div>
             <?php echo form_close(); ?>
       	</div>
--- application/views/institucion/index.php
+++ application/views/institucion/index.php
@@ -1,43 +1,53 @@
+<!------------------ ESTILO DE LAS TABLAS ----------------->
+<link href="<?php echo base_url('resources/css/mitabla.css'); ?>" rel="stylesheet">
+<!-------------------------------------------------------->
+<div class="box-header">
+    <h3 class="box-title">Institución</h3>
+    <div class="box-tools">
+        <?php
+        if($newinst == 0){
+        ?>
+            <a href="<?php echo site_url('institucion/add'); ?>" class="btn btn-success btn-sm">+ Añadir</a>
+        <?php } ?>
+        <?php
+        if($newinst == 1){
+        ?>
+            <a href="<?php echo site_url('institucion/edit/'.$institucion[0]['institucion_id']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Editar</a> 
+        <?php } ?>
+    </div>
+</div>
 <div class="row">
     <div class="col-md-12">
         <div class="box">
-            <div class="box-header">
-                <h3 class="box-title">Institucion Listing</h3>
-            	<div class="box-tools">
-                    <a href="<?php echo site_url('institucion/add'); ?>" class="btn btn-success btn-sm">Add</a> 
-                </div>
-            </div>
+            
             <div class="box-body">
-                <table class="table table-striped">
+                <table class="table table-striped" id="mitabla">
                     <tr>
-						<th>Institucion Id</th>
-						<th>Institucion Nombre</th>
-						<th>Institucion Sucursal</th>
-						<th>Institucion Direccion</th>
-						<th>Institucion Ubicacion</th>
-						<th>Institucion Telef</th>
-						<th>Institucion Nit</th>
-						<th>Institucion Autorizacion</th>
-						<th>Institucion Eslogan</th>
-						<th>Actions</th>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Sucursal</th>
+                        <th>Dirección</th>
+                        <th>Ubicación</th>
+                        <th>Teléfono</th>
+                        <th>Nit</th>
+                        <th>Autorización</th>
+                        <th>Eslogan</th>
                     </tr>
-                    <?php foreach($institucion as $i){ ?>
+                    <?php
+                        $i = 0;
+                        foreach($institucion as $in){ ?>
                     <tr>
-						<td><?php echo $i['institucion_id']; ?></td>
-						<td><?php echo $i['institucion_nombre']; ?></td>
-						<td><?php echo $i['institucion_sucursal']; ?></td>
-						<td><?php echo $i['institucion_direccion']; ?></td>
-						<td><?php echo $i['institucion_ubicacion']; ?></td>
-						<td><?php echo $i['institucion_telef']; ?></td>
-						<td><?php echo $i['institucion_nit']; ?></td>
-						<td><?php echo $i['institucion_autorizacion']; ?></td>
-						<td><?php echo $i['institucion_eslogan']; ?></td>
-						<td>
-                            <a href="<?php echo site_url('institucion/edit/'.$i['institucion_id']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Edit</a> 
-                            <a href="<?php echo site_url('institucion/remove/'.$i['institucion_id']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Delete</a>
-                        </td>
+                        <td><?php echo $i+1; ?></td>
+                        <td><?php echo $in['institucion_nombre']; ?></td>
+                        <td><?php echo $in['institucion_sucursal']; ?></td>
+                        <td><?php echo $in['institucion_direccion']; ?></td>
+                        <td><?php echo $in['institucion_ubicacion']; ?></td>
+                        <td><?php echo $in['institucion_telef']; ?></td>
+                        <td><?php echo $in['institucion_nit']; ?></td>
+                        <td><?php echo $in['institucion_autorizacion']; ?></td>
+                        <td><?php echo $in['institucion_eslogan']; ?></td>
                     </tr>
-                    <?php } ?>
+                    <?php $i++; } ?>
                 </table>
                                 
             </div>
