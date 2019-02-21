2e1c4da096bcca176c7aba1f140b98f07e80c52b
Mario escobar <escobar_m_98@hotmail.com>
2019-02-21 13:49:52 -0400

    Gestión CRUD

--- application/views/gestion/edit.php
+++ application/views/gestion/edit.php
@@ -2,32 +2,40 @@
     <div class="col-md-12">
       	<div class="box box-info">
             <div class="box-header with-border">
-              	<h3 class="box-title">Gestion Edit</h3>
+              	<h3 class="box-title">Editar Gestión</h3>
             </div>
 			<?php echo form_open('gestion/edit/'.$gestion['gestion_id']); ?>
 			<div class="box-body">
 				<div class="row clearfix">
 					<div class="col-md-6">
-						<label for="estado_id" class="control-label">Estado</label>
+						<label for="gestion_nombre" class="control-label">Nombre</label>
 						<div class="form-group">
-							<select name="estado_id" class="form-control">
-								<option value="">select estado</option>
-								<?php 
-								foreach($all_estado as $estado)
-								{
-									$selected = ($estado['estado_id'] == $gestion['estado_id']) ? ' selected="selected"' : "";
-
-									echo '<option value="'.$estado['estado_id'].'" '.$selected.'>'.$estado['estado_descripcion'].'</option>';
-								} 
-								?>
-							</select>
+							<input type="text" name="gestion_nombre" value="<?php echo ($this->input->post('gestion_nombre') ? $this->input->post('gestion_nombre') : $gestion['gestion_nombre']); ?>" class="form-control" id="gestion_nombre" />
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="institucion_id" class="control-label">Institucion</label>
+						<label for="gestion_descripcion" class="control-label">Descripción</label>
+						<div class="form-group">
+							<input type="text" name="gestion_descripcion" value="<?php echo ($this->input->post('gestion_descripcion') ? $this->input->post('gestion_descripcion') : $gestion['gestion_descripcion']); ?>" class="form-control" id="gestion_descripcion" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="gestion_inicio" class="control-label">Inicio</label>
+						<div class="form-group">
+							<input type="date" name="gestion_inicio" value="<?php echo ($this->input->post('gestion_inicio') ? $this->input->post('gestion_inicio') : $gestion['gestion_inicio']); ?>" class="form-control" id="gestion_inicio" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="gestion_fin" class="control-label">Fin</label>
+						<div class="form-group">
+							<input type="date" name="gestion_fin" value="<?php echo ($this->input->post('gestion_fin') ? $this->input->post('gestion_fin') : $gestion['gestion_fin']); ?>" class="form-control" id="gestion_fin" />
+						</div>
+					</div>
+                                        <div class="col-md-6">
+						<label for="institucion_id" class="control-label">Institución</label>
 						<div class="form-group">
 							<select name="institucion_id" class="form-control">
-								<option value="">select institucion</option>
+								<!--<option value="">select institucion</option>-->
 								<?php 
 								foreach($all_institucion as $institucion)
 								{
@@ -39,36 +47,30 @@
 							</select>
 						</div>
 					</div>
-					<div class="col-md-6">
-						<label for="gestion_nombre" class="control-label">Gestion Nombre</label>
+                                        <div class="col-md-6">
+						<label for="estado_id" class="control-label">Estado</label>
 						<div class="form-group">
-							<input type="text" name="gestion_nombre" value="<?php echo ($this->input->post('gestion_nombre') ? $this->input->post('gestion_nombre') : $gestion['gestion_nombre']); ?>" class="form-control" id="gestion_nombre" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="gestion_descripcion" class="control-label">Gestion Descripcion</label>
-						<div class="form-group">
-							<input type="text" name="gestion_descripcion" value="<?php echo ($this->input->post('gestion_descripcion') ? $this->input->post('gestion_descripcion') : $gestion['gestion_descripcion']); ?>" class="form-control" id="gestion_descripcion" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="gestion_inicio" class="control-label">Gestion Inicio</label>
-						<div class="form-group">
-							<input type="text" name="gestion_inicio" value="<?php echo ($this->input->post('gestion_inicio') ? $this->input->post('gestion_inicio') : $gestion['gestion_inicio']); ?>" class="has-datepicker form-control" id="gestion_inicio" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="gestion_fin" class="control-label">Gestion Fin</label>
-						<div class="form-group">
-							<input type="text" name="gestion_fin" value="<?php echo ($this->input->post('gestion_fin') ? $this->input->post('gestion_fin') : $gestion['gestion_fin']); ?>" class="has-datepicker form-control" id="gestion_fin" />
+							<select name="estado_id" class="form-control">
+								<!--<option value="">select estado</option>-->
+								<?php 
+								foreach($all_estado as $estado)
+								{
+									$selected = ($estado['estado_id'] == $gestion['estado_id']) ? ' selected="selected"' : "";
+
+									echo '<option value="'.$estado['estado_id'].'" '.$selected.'>'.$estado['estado_descripcion'].'</option>';
+								} 
+								?>
+							</select>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class="box-footer">
             	<button type="submit" class="btn btn-success">
-					<i class="fa fa-check"></i> Save
-				</button>
+                    <i class="fa fa-check"></i>Guardar
+                </button>
+                <a href="<?php echo site_url('gestion'); ?>" class="btn btn-danger">
+                    <i class="fa fa-times"></i> Cancelar</a>
 	        </div>				
 			<?php echo form_close(); ?>
 		</div>
