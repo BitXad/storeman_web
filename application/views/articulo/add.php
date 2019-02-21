4e987e35480fea05b5c6088b3ec8c3c51f9dd4a0
Mario escobar <escobar_m_98@hotmail.com>
2019-02-21 11:16:00 -0400

    Articulo, genera codigo :D

--- application/views/articulo/add.php
+++ application/views/articulo/add.php
@@ -2,32 +2,47 @@
     <div class="col-md-12">
       	<div class="box box-info">
             <div class="box-header with-border">
-              	<h3 class="box-title">Articulo Add</h3>
+              	<h3 class="box-title">Añadir Artículo</h3>
             </div>
             <?php echo form_open('articulo/add'); ?>
           	<div class="box-body">
           		<div class="row clearfix">
 					<div class="col-md-6">
-						<label for="estado_id" class="control-label">Estado</label>
+						<label for="articulo_nombre" class="control-label"><span class="text-danger">*</span>Nombre</label>
 						<div class="form-group">
-							<select name="estado_id" class="form-control">
-								<option value="">select estado</option>
-								<?php 
-								foreach($all_estado as $estado)
-								{
-									$selected = ($estado['estado_id'] == $this->input->post('estado_id')) ? ' selected="selected"' : "";
-
-									echo '<option value="'.$estado['estado_id'].'" '.$selected.'>'.$estado['estado_descripcion'].'</option>';
-								} 
-								?>
-							</select>
+							<input type="text" name="articulo_nombre" value="<?php echo $this->input->post('articulo_nombre'); ?>" class="form-control" id="articulo_nombre" required />
+                                                        <span class="text-danger"><?php echo form_error('articulo_nombre');?></span>
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="categoria_id" class="control-label">Categoria</label>
+						<label for="articulo_marca" class="control-label">Marca</label>
 						<div class="form-group">
-							<select name="categoria_id" class="form-control">
-								<option value="">select categoria</option>
+							<input type="text" name="articulo_marca" value="<?php echo $this->input->post('articulo_marca'); ?>" class="form-control" id="articulo_marca" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="articulo_industria" class="control-label">Industria</label>
+						<div class="form-group">
+							<input type="text" name="articulo_industria" value="<?php echo $this->input->post('articulo_industria'); ?>" class="form-control" id="articulo_industria" />
+						</div>
+					</div>
+					<!--<div class="col-md-6">
+						<label for="articulo_codigo" class="control-label">Código</label>
+						<div class="form-group">
+							<input type="text" name="articulo_codigo" value="<?php //echo $this->input->post('articulo_codigo'); ?>" class="form-control" id="articulo_codigo" />
+						</div>
+					</div> -->
+					<div class="col-md-6">
+						<label for="articulo_saldo" class="control-label">Saldo</label>
+						<div class="form-group">
+                                                        <input type="number" step="any" min="0" name="articulo_saldo" value="<?php if($this->input->post('articulo_saldo') >0){ echo $this->input->post('articulo_saldo'); }else{ echo "0";} ?>" class="form-control" id="articulo_saldo" />
+						</div>
+					</div>
+                                        <div class="col-md-6">
+						<label for="categoria_id" class="control-label"><span class="text-danger">*</span>Categoría</label>
+						<div class="form-group">
+                                                    <select name="categoria_id" class="form-control" required>
+								<option value="">- CATEGORÍA -</option>
 								<?php 
 								foreach($all_categoria as $categoria)
 								{
@@ -39,42 +54,14 @@
 							</select>
 						</div>
 					</div>
-					<div class="col-md-6">
-						<label for="articulo_nombre" class="control-label">Articulo Nombre</label>
-						<div class="form-group">
-							<input type="text" name="articulo_nombre" value="<?php echo $this->input->post('articulo_nombre'); ?>" class="form-control" id="articulo_nombre" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="articulo_marca" class="control-label">Articulo Marca</label>
-						<div class="form-group">
-							<input type="text" name="articulo_marca" value="<?php echo $this->input->post('articulo_marca'); ?>" class="form-control" id="articulo_marca" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="articulo_industria" class="control-label">Articulo Industria</label>
-						<div class="form-group">
-							<input type="text" name="articulo_industria" value="<?php echo $this->input->post('articulo_industria'); ?>" class="form-control" id="articulo_industria" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="articulo_codigo" class="control-label">Articulo Codigo</label>
-						<div class="form-group">
-							<input type="text" name="articulo_codigo" value="<?php echo $this->input->post('articulo_codigo'); ?>" class="form-control" id="articulo_codigo" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="articulo_saldo" class="control-label">Articulo Saldo</label>
-						<div class="form-group">
-							<input type="text" name="articulo_saldo" value="<?php echo $this->input->post('articulo_saldo'); ?>" class="form-control" id="articulo_saldo" />
-						</div>
-					</div>
 				</div>
 			</div>
           	<div class="box-footer">
             	<button type="submit" class="btn btn-success">
-            		<i class="fa fa-check"></i> Save
+                    <i class="fa fa-check"></i>Guardar
             	</button>
+                <a href="<?php echo site_url('articulo'); ?>" class="btn btn-danger">
+                    <i class="fa fa-times"></i> Cancelar</a>
           	</div>
             <?php echo form_close(); ?>
       	</div>
