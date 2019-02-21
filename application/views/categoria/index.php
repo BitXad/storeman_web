6849aae7e0b4b2f690b233c6573340573faa153e
Mario escobar <escobar_m_98@hotmail.com>
2019-02-21 12:18:18 -0400

    Categoria CRUD

--- application/views/categoria/index.php
+++ application/views/categoria/index.php
@@ -1,33 +1,61 @@
+<!------------------ ESTILO DE LAS TABLAS ----------------->
+<link href="<?php echo base_url('resources/css/mitabla.css'); ?>" rel="stylesheet">
+<!-------------------------------------------------------->
+<div class="box-header">
+    <h3 class="box-title">Categoría</h3>
+    <div class="box-tools">
+        <a href="<?php echo site_url('categoria/add'); ?>" class="btn btn-success btn-sm">+ Añadir</a> 
+    </div>
+</div>
 <div class="row">
     <div class="col-md-12">
         <div class="box">
-            <div class="box-header">
-                <h3 class="box-title">Categoria Listing</h3>
-            	<div class="box-tools">
-                    <a href="<?php echo site_url('categoria/add'); ?>" class="btn btn-success btn-sm">Add</a> 
-                </div>
-            </div>
             <div class="box-body">
-                <table class="table table-striped">
+                <table class="table table-striped" id="mitabla">
                     <tr>
-						<th>Categoria Id</th>
-						<th>Estado Id</th>
-						<th>Categoria Nombre</th>
-						<th>Categoria Descripcion</th>
-						<th>Actions</th>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th>Estado</th>
+                        <th></th>
                     </tr>
-                    <?php foreach($categoria as $c){ ?>
+                    <?php
+                        $i = 0;
+                        foreach($categoria as $c){ ?>
                     <tr>
-						<td><?php echo $c['categoria_id']; ?></td>
-						<td><?php echo $c['estado_id']; ?></td>
-						<td><?php echo $c['categoria_nombre']; ?></td>
-						<td><?php echo $c['categoria_descripcion']; ?></td>
-						<td>
-                            <a href="<?php echo site_url('categoria/edit/'.$c['categoria_id']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Edit</a> 
-                            <a href="<?php echo site_url('categoria/remove/'.$c['categoria_id']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Delete</a>
+                        <td><?php echo $i+1; ?></td>
+                        <td><?php echo $c['categoria_nombre']; ?></td>
+                        <td><?php echo $c['categoria_descripcion']; ?></td>
+                        <td style="background-color: <?php echo $c['estado_color']; ?>"><?php echo $c['estado_descripcion']; ?></td>
+                        <td>
+                            <a href="<?php echo site_url('categoria/edit/'.$c['categoria_id']); ?>" class="btn btn-info btn-xs" title="Modificar"><span class="fa fa-pencil"></span></a> 
+                            <!--<a data-toggle="modal" data-target="#myModal<?php //echo $i; ?>"  title="Eliminar" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span></a>-->
+                            <!------------------------ INICIO modal para confirmar eliminación ------------------->
+                            <div class="modal fade" id="myModal<?php echo $i; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel<?php echo $i; ?>">
+                              <div class="modal-dialog" role="document">
+                                    <br><br>
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">x</span></button>
+                                  </div>
+                                  <div class="modal-body">
+                                   <!------------------------------------------------------------------->
+                                   <h3><b> <span class="fa fa-trash"></span></b>
+                                       ¿Desea eliminar la Categoria <b> <?php echo $c['categoria_nombre']; ?></b>?
+                                   </h3>
+                                   <!------------------------------------------------------------------->
+                                  </div>
+                                  <div class="modal-footer aligncenter">
+                                              <a href="<?php echo site_url('categoria/remove/'.$c['categoria_id']); ?>" class="btn btn-success"><span class="fa fa-check"></span> Si </a>
+                                              <a href="#" class="btn btn-danger" data-dismiss="modal"><span class="fa fa-times"></span> No </a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                        <!------------------------ FIN modal para confirmar eliminación ------------------->
                         </td>
                     </tr>
-                    <?php } ?>
+                    <?php $i++; } ?>
                 </table>
                                 
             </div>
