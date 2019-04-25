283c2b3525c0a7e4674f4440b34a891e84b8e9eb
Mario escobar <escobar_m_98@hotmail.com>
2019-04-25 14:38:54 -0400

    mejorando seguridad

--- application/controllers/Articulo.php
+++ application/controllers/Articulo.php
@@ -12,33 +12,43 @@ class Articulo extends CI_Controller{
         $this->load->model('Articulo_model');
         if ($this->session->userdata('logged_in')) {
             $this->session_data = $this->session->userdata('logged_in');
+        }else {
+            redirect('', 'refresh');
         }
     }
-
+    /* *****Funcion que verifica el acceso al sistema**** */
+    private function acceso($id_rol){
+        $rolusuario = $this->session_data['rol'];
+        if($rolusuario[$id_rol-1]['rolusuario_asignado'] == 1){
+            return;
+        }else{
+            $data['_view'] = 'login/mensajeacceso';
+        $this->load->view('layouts/main',$data);
+        }
+    }
     /*
      * Listing of articulo
      */
     function index()
     {
-        $rolusuario = $this->session_data['rol'];
-        if($rolusuario[1-1]['rolusuario_asignado'] == 1){
-            $data['rolusuario'] = $rolusuario; // <--- es para que se lo use en el index.....
-            $tipo = 1;
-            $data['usuario_nombre'] = "Jacquelinne Alacoria F.";
-            $data['articulo'] = $this->Articulo_model->get_all_articulo();
+        $this->acceso(4);
+        
+        //$data['rolusuario'] = $rolusuario; // <--- es para que se lo use en el index.....
+        $tipo = 1;
+        $data['usuario_nombre'] = "Jacquelinne Alacoria F.";
+        $data['articulo'] = $this->Articulo_model->get_all_articulo();
 
-            $this->load->model('Categoria_model');
-            $data['all_categoria'] = $this->Categoria_model->get_all_categoria();
+        $this->load->model('Categoria_model');
+        $data['all_categoria'] = $this->Categoria_model->get_all_categoria();
 
-            $this->load->model('Estado_model');
-            $data['all_estado'] = $this->Estado_model->get_estado_tipo($tipo);
+        $this->load->model('Estado_model');
+        $data['all_estado'] = $this->Estado_model->get_estado_tipo($tipo);
 
-            $this->load->model('Institucion_model');
-            $data['institucion'] = $this->Institucion_model->get_all_institucion();
+        $this->load->model('Institucion_model');
+        $data['institucion'] = $this->Institucion_model->get_all_institucion();
 
-            $data['_view'] = 'articulo/index';
-            $this->load->view('layouts/main',$data);
-        }
+        $data['_view'] = 'articulo/index';
+        $this->load->view('layouts/main',$data);
     }
 
     /*
@@ -46,51 +56,16 @@ class Articulo extends CI_Controller{
      */
     function add()
     {
-        $rolusuario = $this->session_data['rol'];
-        if($rolusuario[1-1]['rolusuario_asignado'] == 1){
-            $this->load->library('form_validation');
-            $this->form_validation->set_rules('articulo_nombre','Articulo Nombre','trim|required', array('required' => 'Este Campo no debe ser vacio'));
-            if($this->form_validation->run())     
-            {
-                $articulo_nombre = $this->input->post('articulo_nombre');
-                $resultado = $this->Articulo_model->es_articulo_registrado($articulo_nombre);
-                if($resultado > 0){
-                    $data['resultado'] = 1;
-                    $this->load->model('Unidad_manejo_model');
-                    $data['all_unidadmanejo'] = $this->Unidad_manejo_model->get_all_unidad_manejo_activo();
-
-                    $this->load->model('Categoria_model');
-                    $data['all_categoria'] = $this->Categoria_model->get_all_categoria_activo();
-
-                    $data['_view'] = 'articulo/add';
-                    $this->load->view('layouts/main',$data);
-                }else{
-                    //al crear se incia activo
-                    $estado_id = 1;
-                    $params = array(
-                            'estado_id' => $estado_id,
-                            'categoria_id' => $this->input->post('categoria_id'),
-                            'articulo_nombre' => $this->input->post('articulo_nombre'),
-                            'articulo_marca' => $this->input->post('articulo_marca'),
-                            'articulo_industria' => $this->input->post('articulo_industria'),
-                            'articulo_codigo' => $this->input->post('articulo_codigo'),
-                            'articulo_saldo' => $this->input->post('articulo_saldo'),
-                            'articulo_precio' => $this->input->post('articulo_precio'),
-                            'articulo_unidad' => $this->input->post('articulo_unidad'),
-                    );
-                    $articulo_id = $this->Articulo_model->add_articulo($params);
-                    $paramscod = array(
-                            'articulo_codigo' => $this->input->post('categoria_id')."/".$articulo_id,
-                    );
-
-                    $articulo_id = $this->Articulo_model->update_articulo($articulo_id,$paramscod);
-
-                    redirect('articulo');
-                }
-            }
-            else
-            {
-                $data['resultado'] = 0;
+        $this->acceso(4);
+        
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('articulo_nombre','Articulo Nombre','trim|required', array('required' => 'Este Campo no debe ser vacio'));
+        if($this->form_validation->run())     
+        {
+            $articulo_nombre = $this->input->post('articulo_nombre');
+            $resultado = $this->Articulo_model->es_articulo_registrado($articulo_nombre);
+            if($resultado > 0){
+                $data['resultado'] = 1;
                 $this->load->model('Unidad_manejo_model');
                 $data['all_unidadmanejo'] = $this->Unidad_manejo_model->get_all_unidad_manejo_activo();
 
@@ -99,7 +74,41 @@ class Articulo extends CI_Controller{
 
                 $data['_view'] = 'articulo/add';
                 $this->load->view('layouts/main',$data);
+            }else{
+                //al crear se incia activo
+                $estado_id = 1;
+                $params = array(
+                        'estado_id' => $estado_id,
+                        'categoria_id' => $this->input->post('categoria_id'),
+                        'articulo_nombre' => $this->input->post('articulo_nombre'),
+                        'articulo_marca' => $this->input->post('articulo_marca'),
+                        'articulo_industria' => $this->input->post('articulo_industria'),
+                        'articulo_codigo' => $this->input->post('articulo_codigo'),
+                        'articulo_saldo' => $this->input->post('articulo_saldo'),
+                        'articulo_precio' => $this->input->post('articulo_precio'),
+                        'articulo_unidad' => $this->input->post('articulo_unidad'),
+                );
+                $articulo_id = $this->Articulo_model->add_articulo($params);
+                $paramscod = array(
+                        'articulo_codigo' => $this->input->post('categoria_id')."/".$articulo_id,
+                );
+
+                $articulo_id = $this->Articulo_model->update_articulo($articulo_id,$paramscod);
+
+                redirect('articulo');
             }
+        }
+        else
+        {
+            $data['resultado'] = 0;
+            $this->load->model('Unidad_manejo_model');
+            $data['all_unidadmanejo'] = $this->Unidad_manejo_model->get_all_unidad_manejo_activo();
+
+            $this->load->model('Categoria_model');
+            $data['all_categoria'] = $this->Categoria_model->get_all_categoria_activo();
+
+            $data['_view'] = 'articulo/add';
+            $this->load->view('layouts/main',$data);
         }
     }  
 
@@ -108,50 +117,49 @@ class Articulo extends CI_Controller{
      */
     function edit($articulo_id)
     {
-        $rolusuario = $this->session_data['rol'];
-        if($rolusuario[1-1]['rolusuario_asignado'] == 1){
-            // check if the articulo exists before trying to edit it
-            $data['articulo'] = $this->Articulo_model->get_articulo($articulo_id);
+        $this->acceso(4);
+        
+        // check if the articulo exists before trying to edit it
+        $data['articulo'] = $this->Articulo_model->get_articulo($articulo_id);
 
-            if(isset($data['articulo']['articulo_id']))
+        if(isset($data['articulo']['articulo_id']))
+        {
+            $this->load->library('form_validation');
+            $this->form_validation->set_rules('articulo_nombre','Articulo Nombre','trim|required', array('required' => 'Este Campo no debe ser vacio'));
+            if($this->form_validation->run())     
             {
-                $this->load->library('form_validation');
-                $this->form_validation->set_rules('articulo_nombre','Articulo Nombre','trim|required', array('required' => 'Este Campo no debe ser vacio'));
-                if($this->form_validation->run())     
-                {
-                    $params = array(
-                                            'estado_id' => $this->input->post('estado_id'),
-                                            'categoria_id' => $this->input->post('categoria_id'),
-                                            'articulo_nombre' => $this->input->post('articulo_nombre'),
-                                            'articulo_marca' => $this->input->post('articulo_marca'),
-                                            'articulo_industria' => $this->input->post('articulo_industria'),
-                                            'articulo_codigo' => $this->input->post('articulo_codigo'),
-                                            'articulo_saldo' => $this->input->post('articulo_saldo'),
-                                            'articulo_precio' => $this->input->post('articulo_precio'),
-                                            'articulo_unidad' => $this->input->post('articulo_unidad'),
-                    );
+                $params = array(
+                                        'estado_id' => $this->input->post('estado_id'),
+                                        'categoria_id' => $this->input->post('categoria_id'),
+                                        'articulo_nombre' => $this->input->post('articulo_nombre'),
+                                        'articulo_marca' => $this->input->post('articulo_marca'),
+                                        'articulo_industria' => $this->input->post('articulo_industria'),
+                                        'articulo_codigo' => $this->input->post('articulo_codigo'),
+                                        'articulo_saldo' => $this->input->post('articulo_saldo'),
+                                        'articulo_precio' => $this->input->post('articulo_precio'),
+                                        'articulo_unidad' => $this->input->post('articulo_unidad'),
+                );
 
-                    $this->Articulo_model->update_articulo($articulo_id,$params);            
-                    redirect('articulo');
-                }
-                else
-                {
-                    $this->load->model('Unidad_manejo_model');
-                    $data['all_unidadmanejo'] = $this->Unidad_manejo_model->get_all_unidad_manejo_activo();
-
-                    $this->load->model('Estado_model');
-                    $data['all_estado'] = $this->Estado_model->get_all_estado_tipo1();
-
-                    $this->load->model('Categoria_model');
-                    $data['all_categoria'] = $this->Categoria_model->get_all_categoria_activo();
-
-                    $data['_view'] = 'articulo/edit';
-                    $this->load->view('layouts/main',$data);
-                }
+                $this->Articulo_model->update_articulo($articulo_id,$params);            
+                redirect('articulo');
             }
             else
-                show_error('The articulo you are trying to edit does not exist.');
+            {
+                $this->load->model('Unidad_manejo_model');
+                $data['all_unidadmanejo'] = $this->Unidad_manejo_model->get_all_unidad_manejo_activo();
+
+                $this->load->model('Estado_model');
+                $data['all_estado'] = $this->Estado_model->get_all_estado_tipo1();
+
+                $this->load->model('Categoria_model');
+                $data['all_categoria'] = $this->Categoria_model->get_all_categoria_activo();
+
+                $data['_view'] = 'articulo/edit';
+                $this->load->view('layouts/main',$data);
+            }
         }
+        else
+            show_error('The articulo you are trying to edit does not exist.');
     }
     
     /* buscar los articulos */
@@ -174,44 +182,44 @@ class Articulo extends CI_Controller{
      */
     function remove()
     {
-        $rolusuario = $this->session_data['rol'];
-        if($rolusuario[1-1]['rolusuario_asignado'] == 1){
-            $articulo_id = $this->input->post('articulo_id');
-            $articulo = $this->Articulo_model->get_articulo($articulo_id);
+        $this->acceso(5);
+        
+        $articulo_id = $this->input->post('articulo_id');
+        $articulo = $this->Articulo_model->get_articulo($articulo_id);
 
-            // check if the programa exists before trying to delete it
-            if(isset($articulo['articulo_id']))
-            {
-                $this->Articulo_model->delete_articulo($articulo_id);
-                echo json_encode("ok");
-            }
-            else
-                echo json_encode("no");
+        // check if the programa exists before trying to delete it
+        if(isset($articulo['articulo_id']))
+        {
+            $this->Articulo_model->delete_articulo($articulo_id);
+            echo json_encode("ok");
         }
+        else
+            echo json_encode("no");
     }
     /*
      * Inactivar Articulo
      */
     function inactivar()
     {
-        $rolusuario = $this->session_data['rol'];
-        if($rolusuario[1-1]['rolusuario_asignado'] == 1){
-            $articulo_id = $this->input->post('articulo_id');
-            $articulo = $this->Articulo_model->get_articulo($articulo_id);
+        $this->acceso(4);
+        
+        $articulo_id = $this->input->post('articulo_id');
+        $articulo = $this->Articulo_model->get_articulo($articulo_id);
 
-            // check if the programa exists before trying to delete it
-            if(isset($articulo['articulo_id']))
-            {
-                $this->Articulo_model->inactivar_articulo($articulo_id);
-                echo json_encode("ok");
-            }
-            else
-                echo json_encode("no");
+        // check if the programa exists before trying to delete it
+        if(isset($articulo['articulo_id']))
+        {
+            $this->Articulo_model->inactivar_articulo($articulo_id);
+            echo json_encode("ok");
         }
+        else
+            echo json_encode("no");
     }
     /* muestra los 50 primeros en orden alfabetico */
     function buscararticulolimit()
     {
+        $this->acceso(4);
+        
         if ($this->input->is_ajax_request())
         {
             $datos = $this->Articulo_model->get_all_articulolimit();
@@ -226,6 +234,8 @@ class Articulo extends CI_Controller{
     /* buscar los articulos */
     function buscartodoslosarticulos()
     {
+        $this->acceso(4);
+        
         if ($this->input->is_ajax_request())
         {
             $parametro = $this->input->post('parametro');
---
+++ application/views/login/mensajeacceso.php
@@ -0,0 +1,12 @@
+<div id="myModal" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2 class="modal-title text-danger text-center">No tiene permisos para usar este modulo</h2>
+            </div>
+            <div class="modal-body">
+              <p><b>Por favor consulte con su adminsitrador</b></p>
+            </div>
+        </div>
+    </div>
+</div>
